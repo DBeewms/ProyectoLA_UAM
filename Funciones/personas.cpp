eff0455bb151bf6d6b7fcf0e35df473c5417dbf7
DBeewms <diego.mora16063@est.uca.edu.ni>
2024-05-12 19:12:58 -0600

    Se agregaron nuevos vectores para asistencias y entradas tarde, se movio la funcion buscarEmpleado

--- Funciones/personas.cpp
+++ Funciones/personas.cpp
@@ -4,7 +4,34 @@
 
 using namespace std;
 
+// Buscar empleados
+void buscarEmpleado();
+
+vector<int> asistencias;
+vector<int> tarde;
 vector<string> nombres;
 vector<string> apellidos;
 vector<string> cedulas;
-vector<string> contraseñas;
+vector<string> claves;
+
+int i = 0;
+
+void buscarEmpleado()
+{
+    i = 0;
+    string cedula;
+    cout << "Ingrese la cedula del empleado que desea buscar: ";
+    cin >> cedula;
+
+    for (i < cedulas.size(); i++;)
+    {
+        if (cedulas[i] == cedula)
+        {
+            break;
+        }
+        else
+        {
+            cout << "Empleado no encontrado" << endl;
+        }
+    }
+}
