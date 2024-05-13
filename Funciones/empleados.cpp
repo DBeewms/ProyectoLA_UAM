4d6534adec35feb4c71380d4e5e2d8447966e257
DBeewms <diego.mora16063@est.uca.edu.ni>
2024-05-12 19:15:21 -0600

    Se desarrollaron funciones iniciarSesionEmpleado y registrarAsistencia

--- Funciones/empleados.cpp
+++ Funciones/empleados.cpp
@@ -1,17 +1,60 @@
-#include "inicioSesion.cpp"
+#include "admin.cpp"
+#include <iostream>
+#include <chrono>
+#include <ctime>
 
-//Uso de la funcion generarContrasena
+using namespace std;
+
+int i = 0;
+
+void iniciarSesionEmpleado()
+{
+    char contrasena[sizePass];
+    buscarEmpleado();
+
+    cout << "Ingrese la contraseña: ";
+    cin >> contrasena;
+
+    if (claves[i] == contrasena)
+    {
+        cout << "Sesion iniciada" << endl;
+    }
+    else
+    {
+        cout << "Contraseña incorrecta" << endl;
+    }
+}
+
+void registrarAsistencia()
+{
+
+    switch (expression)
+    {
+    case /* constant-expression */:
+        /* code */
+        break;
+    
+    default:
+        break;
+    }
+    
+    cout << "Nombre: " << nombres[i] << endl;
+    cout << "Apellido: " << apellidos[i] << endl;
+    cout << "Cedula: " << cedulas[i] << endl;
+
+}
+
+// Uso de la funcion generarContrasena
 
 // int main(int argc, char const *argv[])
 // {
 //     char contrasena[sizePass+1];
-    
+
 //     generarContrasena(contrasena);
 //     for (int i = 0; i < sizePass; i++)
 //     {
 //         cout << contrasena[i];
 //     }
-    
-    
+
 //     return 0;
 // }
