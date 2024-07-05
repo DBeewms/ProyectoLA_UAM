33d6af642aee54b3f683f244b30163e3da9d6cef
DBeewms <dbwcontrol05@gmail.com>
2024-07-04 19:02:10 -0600

    Se desarrollaron funciones para ingresarEntradaSalida y sus funciones para guardar y leer en fichero, y tambien la funcion para crear informes mensuales del empleado

--- Funciones/informes.cpp
+++ Funciones/informes.cpp
@@ -4,24 +4,23 @@
 #include <chrono>
 #include <fstream>
 
+#define ANIOACTUAL 2024
+
 using namespace std;
 using namespace chrono;
 
 time_t horaEntrada_t, horaSalida_t;
 char horaEntrada_str[9], horaSalida_str[9];
 
-    // Caso de uso
+// Funciones para el informe
+void ingresarEntradaSalida();
+void guardarHoras();
+void leerHoras();
+int zeller(int anio, int mes, int dia);
+int calcularDiasDelMes(int mes, int anio);
 
-    // ingresarEntradaSalida();
-
-    // // Guardar las horas en un archivo
-    // guardarHoras();
-
-    // // Leer las horas desde el archivo
-    // leerHoras();
-    // cout << "Hora de entrada: " << horaEntrada_str << endl;
-    // cout << "Hora de salida: " << horaSalida_str << endl;
-
+// Cuantificar las asistencias y llegadas tardes en un mes
+void cuantificarAsistenciasYLlegadasTardes(int mes, int anio, int indexEmpleado);
 
 void ingresarEntradaSalida()
 {
@@ -34,12 +33,26 @@ void ingresarEntradaSalida()
     // Convertir a una estructura tm que representa la fecha y tiempo de un calendario
     tm *now_tm = localtime(&now_time_t);
 
-    // Pedir hora de entrada y de salida
     cout << "Define la hora de entrada y salida..." << endl;
-    cout << "Hora de entrada (HH:MM:SS): ";
-    cin.getline(horaEntrada_str, 9);
-    cout << "Hora de salida (HH:MM:SS): ";
-    cin.getline(horaSalida_str, 9);
+    do
+    {
+        cout << "Hora de entrada (HH:MM:SS): ";
+        cin >> horaEntrada_str;
+        if (horaEntrada_str[0] == '\0')
+        { // Si la cadena está vacía, limpia el buffer
+            cin.ignore(10000, '\n');
+        }
+    } while (horaEntrada_str[0] == '\0'); // Repite si la cadena está vacía
+
+    do
+    {
+        cout << "Hora de salida (HH:MM:SS): ";
+        cin >> horaSalida_str;
+        if (horaSalida_str[0] == '\0')
+        { // Si la cadena está vacía, limpia el buffer
+            cin.ignore(10000, '\n');
+        }
+    } while (horaSalida_str[0] == '\0'); // Repite si la cadena está vacía
 
     // Parsear la hora de entrada y salida a tm
     tm horaEntrada_tm = *now_tm, horaSalida_tm = *now_tm;
@@ -52,10 +65,12 @@ void ingresarEntradaSalida()
 }
 
 // Función para guardar las horas en un archivo usando char[]
-void guardarHoras() {
+void guardarHoras()
+{
     ofstream file("horaEntradaSalida.txt");
 
-    if (file.is_open()) {
+    if (file.is_open())
+    {
         // Guardar las representaciones de cadena
         file << horaEntrada_str << "\n";
         file << horaSalida_str << "\n";
@@ -63,87 +78,82 @@ void guardarHoras() {
         file << horaEntrada_t << "\n";
         file << horaSalida_t << "\n";
         file.close();
-    } else {
+    }
+    else
+    {
         cout << "No se pudo abrir el archivo para escribir." << endl;
     }
 }
 
 // Función para leer las horas desde un archivo usando char[] y time_t
-void leerHoras() {
+void leerHoras()
+{
     ifstream file("horaEntradaSalida.txt");
 
-    if (file.is_open()) {
+    if (file.is_open())
+    {
         file.getline(horaEntrada_str, sizeof(horaEntrada_str));
         file.getline(horaSalida_str, sizeof(horaSalida_str));
         // Leer los valores time_t
         file >> horaEntrada_t;
         file >> horaSalida_t;
         file.close();
-    } else {
+    }
+    else
+    {
         cout << "No se pudo abrir el archivo para leer." << endl;
     }
 }
 
+void cuantificarAsistenciasYLlegadasTardes(int mes, int anio, int indexEmpleado)
+{
+    int totalAsistencias = 0;
+    int totalLlegadasTardes = 0;
+    anio = ANIOACTUAL;
+    int diasDelMes = calcularDiasDelMes(mes, anio);
 
-// // Usar estas variables al implementar las funciones:
+    for (int i = 0; i < MAX_FECHAS; i++)
+    {
+        if (empleados[indexEmpleado].arreglosFechas[i].mesDeFecha == mes)
+        {
+            totalAsistencias += empleados[indexEmpleado].arreglosFechas[i].asistencia;
+            totalLlegadasTardes += empleados[indexEmpleado].arreglosFechas[i].llegadaEmpleado;
+        }
+    }
 
-// // time_t horaEntrada_t, horaSalida_t;
-// // pair<time_t, time_t> horasEntradaSalida;
+    // Creación del nombre del archivo
+    char nombreArchivo[60];
+    strcpy(nombreArchivo, "informe");
+    strcat(nombreArchivo, empleados[indexEmpleado].infoUsuario.cedula); // Concatenar la cédula
+    strcat(nombreArchivo, ".txt");                                      // Concatenar la extensión del archivo
 
-// // // Funciones
-// // void pedirFecha();
+    // Creación y apertura del archivo
+    ofstream archivo(nombreArchivo);
 
-// // Establecer la hora de entrada y salida de un empleado
-// pair<time_t, time_t> ingresarEntradaSalida();
+    if (archivo.is_open())
+    {
+        // Escribir datos en el archivo
+        archivo << "Nombre: " << empleados[indexEmpleado].nombre << " " << empleados[indexEmpleado].apellido << endl;
+        archivo << "Mes: " << mes << " Año: " << anio << endl;
+        archivo << "Días del mes: " << diasDelMes << endl;
+        archivo << "Total de asistencias en el mes: " << totalAsistencias << endl;
+        archivo << "Total de llegadas tardes en el mes: " << totalLlegadasTardes << endl;
+        archivo << "Proporción de asistencias: " << totalAsistencias << "/" << diasDelMes << endl;
+        archivo.close(); // Cerrar el archivo
+    }
+    else
+    {
+        cout << "No se pudo abrir el archivo para escritura." << endl;
+    }
 
-// // //Calcular las horas extras de un empleado
-// // time_t calcularHorasExtra(time_t horaSalida_t);
-
-// int zeller(int anio, int mes, int dia);
-// int calcularDiasMes(int mes, int anio);
-
-// pair<time_t, time_t> ingresarEntradaSalida()
-// {
-//     string horaEntrada_str, horaSalida_str;
-
-//     // Tomar el punto actual del calendario del sistema
-//     auto now = system_clock::now();
-
-//     // Convertir a time_t que representa el tiempo del calendario
-//     time_t now_time_t = system_clock::to_time_t(now);
-
-//     /*Convertir un tiempo en formato time_t a una estructura tm que representa la fecha
-//     y hora local, permitiendo un acceso más fácil a cada componente de la fecha y hora.*/
-//     tm *now_tm = localtime(&now_time_t);
-
-//     // Pedir hora de entrada y de salida
-//     cout << "Define la hora de entrada y salida..." << endl;
-//     cout << "Hora de entrada (HH:MM:SS): ";
-//     cin >> horaEntrada_str;
-//     cout << "Hora de salida (HH:MM:SS): ";
-//     cin >> horaSalida_str;
-
-//     // Parsear la hora de entrada y salida a tm
-//     tm horaEntrada_tm = *now_tm, horaSalida_tm = *now_tm; // Creacion e inicializacion de variables tipo tm
-//     stringstream horaEntrada_ss(horaEntrada_str), horaSalida_ss(horaSalida_str);
-
-//     /*Utilizan el operador de extracción (>>) para leer datos de los objetos stringstream*/
-//     horaEntrada_ss >> get_time(&horaEntrada_tm, "%H:%M:%S");
-//     horaSalida_ss >> get_time(&horaSalida_tm, "%H:%M:%S");
-//     /*La función get_time se utiliza para interpretar los datos leídos de
-//     acuerdo con el formato especificado ("%H:%M:%S")*/
-
-//     // Parsear las estructuras tm devuelta a time_t
-//     time_t horaEntrada_t = mktime(&horaEntrada_tm);
-//     time_t horaSalida_t = mktime(&horaSalida_tm);
-
-//     /*Por que no pasar directamente a time_t?
-//     Se obtiene la validación y normalización de la fecha y hora automáticamente.
-//     Se ajustan los valores de tm si son incoherentes,
-//     y esto no es algo que se obtendría fácilmente parseando directamente a time_t.*/
-
-//     return make_pair(horaEntrada_t, horaSalida_t); // Devuelve un PAR de valores
-// }
+    // También mostrar los datos en consola
+    cout << "Nombre: " << empleados[indexEmpleado].nombre << " " << empleados[indexEmpleado].apellido << endl;
+    cout << "Mes: " << mes << " Año: " << anio << endl;
+    cout << "Días del mes: " << diasDelMes << endl;
+    cout << "Total de asistencias en el mes: " << totalAsistencias << endl;
+    cout << "Total de llegadas tardes en el mes: " << totalLlegadasTardes << endl;
+    cout << "Proporción de asistencias: " << totalAsistencias << "/" << diasDelMes << endl;
+}
 
 // Devuelve el dia de la semana
 int zeller(int anio, int mes, int dia)
@@ -158,57 +168,21 @@ int zeller(int anio, int mes, int dia)
 }
 
 // Devuelve la cantidad de dias dependiendo del mes
-int calcularDiasMes(int mes, int anio)
+int calcularDiasDelMes(int mes, int anio)
 {
-    int dias = 0;
-
     switch (mes)
     {
-    case 1:
-        dias = 31;
-        break;
-    case 2:
-        if (anio % 4 == 0 && (anio % 100 != 0 || anio % 400 == 0))
-        {
-            dias = 29;
-        }
-        else
-        {
-            dias = 28;
-        }
-        break;
-    case 3:
-        dias = 31;
-        break;
     case 4:
-        dias = 30;
-        break;
-    case 5:
-        dias = 31;
-        break;
     case 6:
-        dias = 30;
-        break;
-    case 7:
-        dias = 31;
-        break;
-    case 8:
-        dias = 31;
-        break;
     case 9:
-        dias = 30;
-        break;
-    case 10:
-        dias = 31;
-        break;
     case 11:
-        dias = 30;
-        break;
-    case 12:
-        dias = 31;
-        break;
+        return 30;
+    case 2:
+        if ((anio % 4 == 0 && anio % 100 != 0) || (anio % 400 == 0))
+            return 29;
+        else
+            return 28;
     default:
-        break;
+        return 31;
     }
-    return dias;
 }
